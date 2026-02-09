2583b37a994c7805857faf3f088c9ea1922aba03
Michael Menzies <michael@virtualdesigns.co.za>
2026-02-09 11:26:56 +0200

    Changed routes/api.php to match old CMS /api/v2/

--- routes/api.php
+++ routes/api.php
@@ -19,23 +19,27 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('properties')->group(function () {
-    Route::get('/', [PropertiesController::class, 'index']);
-    Route::get('/user/{userid}', [PropertiesController::class, 'show']);
-    Route::get('/{id}/detail', [PropertiesController::class, 'getSingleProperty']);
-    Route::post('/', [PropertiesController::class, 'store']);
-    Route::put('/{id}', [PropertiesController::class, 'update']);
-    Route::get('/types', [PropertiesController::class, 'getTypes']);
-    Route::get('/{id}/configuration', [PropertiesController::class, 'getConfiguration']);
-    Route::put('/{id}/configuration', [PropertiesController::class, 'updateConfiguration']);
-    Route::put('/{id}/owner', [PropertiesController::class, 'updateOwnerDetails']);
-    Route::post('/{id}/owner-documents', [PropertiesController::class, 'uploadOwnerDocuments']);
-    Route::delete('/owner-documents/{documentId}', [PropertiesController::class, 'deleteOwnerDocuments']);
-    Route::get('/serp', [PropertiesController::class, 'serp']);
-    Route::put('/{id}/live', [PropertiesController::class, 'onClickLive']);
-    Route::put('/{id}/lock', [PropertiesController::class, 'onClickLock']);
-    Route::post('/rentals-united/sync', [PropertiesController::class, 'syncWithRentalsUnited']);
-    Route::get('/rentals-united', [PropertiesController::class, 'getRentalsUnitedProperties']);
-    Route::post('/welcome-letter-preview', [PropertiesController::class, 'sendWelcomeLetterPreview']);
-    Route::post('/{id}/sync-rates', [PropertiesController::class, 'SyncRatesAvail']);
+Route::prefix('v2')->group(function () {
+    Route::prefix('properties')->group(function () {
+        Route::get('serp', [PropertiesController::class, 'serp']);
+        Route::get('single/{id}', [PropertiesController::class, 'getSingleProperty']);
+        Route::put('update-owner-details/{id}', [PropertiesController::class, 'updateOwnerDetails']);
+        Route::post('upload-owner-documents/{id}', [PropertiesController::class, 'uploadOwnerDocuments']);
+        Route::put('uploadpdf/{id}', [PropertiesController::class, 'uploadpdf']);
+        Route::put('update/{id}', [PropertiesController::class, 'update']);
+        Route::put('get-images/{id}', [PropertiesController::class, 'getImages']);
+        Route::post('onclicklive/{id}', [PropertiesController::class, 'onClickLive']);
+        Route::post('onclicklock/{id}', [PropertiesController::class, 'onClickLock']);
+        Route::delete('delete/{id}', [PropertiesController::class, 'destroy']);
+        Route::delete('delete-owner-document/{documentId}', [PropertiesController::class, 'deleteOwnerDocuments']);
+        Route::post('sync-with-rentals-united', [PropertiesController::class, 'syncWithRentalsUnited']);
+        Route::post('update-configuration/{id}', [PropertiesController::class, 'updateConfiguration']);
+        Route::get('get-configuration/{id}', [PropertiesController::class, 'getConfiguration']);
+        Route::get('get-rentals-united-properties', [PropertiesController::class, 'getRentalsUnitedProperties']);
+        Route::get('gettypes', [PropertiesController::class, 'getTypes']);
+        Route::get('sendwelcomeletterpreview', [PropertiesController::class, 'sendWelcomeLetterPreview']);
+        Route::get('syncratesavail/{id}', [PropertiesController::class, 'SyncRatesAvail']);
+    });
+
+    Route::resource('properties', PropertiesController::class);
 });
