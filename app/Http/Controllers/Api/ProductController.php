2d06b49006e94fb2d57dcb20643737d7b7f1dbe9
Ryno <ryno@hostagents.com>
2026-02-12 16:21:36 +0200

    feat: update controller

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -73,11 +73,11 @@ class ProductController extends Controller
 
     /**
      * Product data that can be use to visualized data.
-    */
+     */
     public function storeBodyCorp(Request $request)
     {
         try {
-            
+
             $this->assertApiKey($request);
             Log::debug($request);
 
@@ -134,8 +134,8 @@ class ProductController extends Controller
             $body_corp_emails = array();
             $emails_inc = 0;
 
-            if(isset($request->body_corp_email) && $request->body_corp_email !== "" ) {
-                    array_push($body_corp_emails, $request->body_corp_email);
+            if (isset($request->body_corp_email) && $request->body_corp_email !== "") {
+                array_push($body_corp_emails, $request->body_corp_email);
             }
             // while ($emails_inc <= $request->body_corp_emails_count) {
             //     $body_corp_email = $request->input('body_corp_email_' . $emails_inc);
@@ -192,14 +192,168 @@ class ProductController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        try {
+            if ($request->body_corp_full_names_required == 1) {
+                $body_corp_full_names_required = 1;
+            } else {
+                $body_corp_full_names_required = 0;
+            }
+
+            if ($request->body_corp_vehicle_reg_required == 1) {
+                $body_corp_vehicle_reg_required = 1;
+            } else {
+                $body_corp_vehicle_reg_required = 0;
+            }
+
+            if ($request->body_corp_id_selfies_required == 1) {
+                $body_corp_id_selfies_required = 1;
+            } else {
+                $body_corp_id_selfies_required = 0;
+            }
+
+            if ($request->body_corp_all_guest_contacts_required == 1) {
+                $body_corp_all_guest_contacts_required = 1;
+            } else {
+                $body_corp_all_guest_contacts_required = 0;
+            }
+
+            if ($request->body_corp_all_guest_id_img_required == 1) {
+                $body_corp_all_guest_id_img_required = 1;
+            } else {
+                $body_corp_all_guest_id_img_required = 0;
+            }
+
+            if ($request->body_corp_main_guest_name_and_phone_number_required == 1) {
+                $body_corp_main_guest_name_and_phone_number_required = 1;
+            } else {
+                $body_corp_main_guest_name_and_phone_number_required = 0;
+            }
+
+            if ($request->main_guest_name_phone_number_and_id_number_image_upload_required == 1) {
+                $main_guest_name_phone_number_and_id_number_image_upload_required = 1;
+            } else {
+                $main_guest_name_phone_number_and_id_number_image_upload_required = 0;
+            }
+
+
+            if ($request->body_corp_to_send == 1) {
+                $body_corp_to_send = 1;
+            } else {
+                $body_corp_to_send = 0;
+            }
+
+            $body_corp_emails = array();
+            $emails_inc = 0;
+            while ($emails_inc <= $request->body_corp_emails_count) {
+                $body_corp_email = $request->input('body_corp_email_' . $emails_inc);
+                array_push($body_corp_emails, $body_corp_email);
+                $emails_inc = $emails_inc + 1;
+            }
+
+            if ($body_corp_id != null && $body_corp_id != "") {
+                DB::connection('remote_test')->table('virtualdesigns_bodycorp_bodycorp')
+                    ->where('id', '=', $body_corp_id)
+                    ->where('deleted_at', '=', null)
+                    ->update([
+                        "rule_name" => $request->rule_name,
+                        "body_corp_name" => $request->body_corp_name,
+                        "body_corp_phone" => $request->body_corp_phone,
+                        "body_corp_contact_person" => $request->body_corp_contact_person,
+                        "body_corp_emails" => json_encode($body_corp_emails),
+                        "notes" => $request->body_corp_notes,
+                        "body_corp_full_names_required" => $body_corp_full_names_required,
+                        "body_corp_to_send" => $body_corp_to_send,
+                        "body_corp_vehicle_reg_required" => $body_corp_vehicle_reg_required,
+                        "body_corp_id_selfies_required" => $body_corp_id_selfies_required,
+                        "body_corp_all_guest_contacts_required" => $body_corp_all_guest_contacts_required,
+                        "body_corp_all_guest_id_img_required" => $body_corp_all_guest_id_img_required,
+                        "body_corp_main_guest_name_and_phone_number_required" => $body_corp_main_guest_name_and_phone_number_required,
+                        "main_guest_name_phone_number_and_id_number_image_upload_required" => $main_guest_name_phone_number_and_id_number_image_upload_required,
+                        "created_at" => date("Y-m-d H:i:s")
+                    ]);
+                $message_sent = "Guest Checkin Rule Updated Successfully";
+            } else {
+                $exists = DB::connection('remote_test')->table('virtualdesigns_bodycorp_bodycorp')
+                    ->where('virtualdesigns_bodycorp_bodycorp.deleted_at', '=', null)
+                    ->where("rule_name", $request->rule_name)->exists();
+
+
+                if ($request->rule_name !== "" && !$exists) {
+                    DB::connection('remote_test')->table('virtualdesigns_bodycorp_bodycorp')
+                        ->insert([
+                            "rule_name" => $request->rule_name,
+                            "body_corp_name" => $request->body_corp_name,
+                            "body_corp_phone" => $request->body_corp_phone,
+                            "body_corp_contact_person" => $request->body_corp_contact_person,
+                            "body_corp_emails" => json_encode($body_corp_emails),
+                            "notes" => $request->body_corp_notes,
+                            "body_corp_to_send" => $body_corp_to_send,
+                            "body_corp_full_names_required" => $body_corp_full_names_required,
+                            "body_corp_vehicle_reg_required" => $body_corp_vehicle_reg_required,
+                            "body_corp_id_selfies_required" => $body_corp_id_selfies_required,
+                            "body_corp_all_guest_contacts_required" => $body_corp_all_guest_contacts_required,
+                            "body_corp_all_guest_id_img_required" => $body_corp_all_guest_id_img_required,
+                            "body_corp_main_guest_name_and_phone_number_required" => $body_corp_main_guest_name_and_phone_number_required,
+                            "main_guest_name_phone_number_and_id_number_image_upload_required" => $main_guest_name_phone_number_and_id_number_image_upload_required,
+                            "created_at" => date("Y-m-d H:i:s")
+                        ]);
+                    $message_sent = "New Guest Checkin Rule Created";
+                } else {
+                    $message_sent = "Rule name already exists!";
+                }
+            }
+            $body_corps = DB::connection('remote_test')
+                ->table('virtualdesigns_bodycorp_bodycorp')
+                ->where('virtualdesigns_bodycorp_bodycorp.deleted_at', '=', null)
+                ->get();
+
+            $emails = DB::connection('remote_test')
+                ->table('virtualdesigns_bodycorp_emails')
+                ->get()
+                ->groupBy('bodycorp_id');
+
+            $body_corps = $body_corps->map(function ($corp) use ($emails) {
+                $corp->emails = $emails->get($corp->id, collect())->toArray();
+                return $corp;
+            });
+            // Redirect Problem that needs to be solved.
+            view("pages.product.check-in-rules", ['message_sent' => $message_sent, 'body_corps' => $body_corps]);
+            return redirect()->back()->with('message_sent', $message_sent);
+        } catch (\Throwable $e) {
+            if ($e instanceof HttpResponseException) {
+                return $e->getResponse();
+            }
+            return $this->corsJson(['error' => $e->getMessage(), "message" => "Failed to update Body Corporate By Id"], 500);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Request $request, string $id)
     {
-        //
+        try {
+            if ($request->deleted) {
+                $delete_body_corps = DB::table('virtualdesigns_bodycorp_bodycorp')
+                    ->where('id', '=', $id)
+                    ->update(['deleted_at' => date("Y-m-d H:i:s")]);
+
+
+                $bodycorps = DB::connection('remote_test')
+                    ->table('virtualdesigns_bodycorp_bodycorp')
+                    ->where('deleted_at', '=', null)
+                    ->get();
+
+                $message_sent = "Body Corporate Deleted Succesfully";
+                if ($delete_body_corps === 1) {
+                    return $this->corsJson(["message" => $message_sent, "deleted" => true, "bodycorps" => $bodycorps], 200);
+                }
+            }
+        } catch (\Throwable $e) {
+            if ($e instanceof HttpResponseException) {
+                return $e->getResponse();
+            }
+            return $this->corsJson(['error' => $e->getMessage(), "message" => "Failed to deleted Body Corporate By Id"], 500);
+        }
     }
 }
